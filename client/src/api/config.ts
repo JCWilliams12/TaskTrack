e3bd6d600e4627c61f1da13274a054572442e979
JCWilliams12 <JCWilliams3433@gmail.com>
2025-09-25 10:36:25 -0500

    Changed base URL to allow API

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 
 const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}/api`,
 });
 
 export default axiosInstance;
