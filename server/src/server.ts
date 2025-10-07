30836ea7025314edc1d1b531fe821d916261ecfa
JCWilliams12 <JCWilliams3433@gmail.com>
2025-10-07 09:28:01 -0500

    updated perm url

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ dotenv.config();
 
 // CORS Configuration
 // List of allowed origins (your frontend's URL)
-const allowedOrigins = ['https://task-track-jmybppupu-jcwilliams12s-projects.vercel.app'];
+const allowedOrigins = ['task-track-pi-eight.vercel.app'];
 //aibdvawijdbv
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
